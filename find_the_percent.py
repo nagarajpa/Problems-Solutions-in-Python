a6a1f7da487e3974d59a51f6799a73e09041ba3c
Nagaraj Putti <nagarajpa@nagarajpa-macbookpro.roam.corp.google.com>
2019-02-17 14:44:59 +0530

    find_the_percent.py

---
+++ find_the_percent.py
@@ -0,0 +1,32 @@
+"""
+You have a record of  students. Each record contains the student's name, and their percent marks in Maths, Physics and Chemistry. The marks can be floating values. The user enters some integer  followed by the names and marks for students. You are required to save the record in a dictionary data type. The user then enters a student's name. Output the average percentage marks obtained by that student, correct to two decimal places.
+
+Input Format
+
+The first line contains the integer , the number of students. The next  lines contains the name and marks obtained by that student separated by a space. The final line contains the name of a particular student previously listed.
+
+Constraints
+
+Output Format
+
+Print one line: The average of the marks obtained by the particular student correct to 2 decimal places.
+
+https://www.hackerrank.com/challenges/finding-the-percentage/problem
+"""
+
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    scores_new = student_marks[query_name]
+    sum = 0
+    for x in scores_new:
+      sum += x
+    avg = sum /len(scores_new)
+    print('{0:.2f}'.format(avg))
+
